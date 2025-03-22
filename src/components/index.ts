9184a4cfe922626965e3bd4d84abce032782dd6f
backsty <shalamovartem6@mail.ru>
2025-03-23 03:34:39 +0700

    fix: import AdvancedFilter

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,4 @@ export { ItemForm } from './item-form';
 export { ItemList } from './item-list';
 export { ItemRow } from './item-row';
 export { Filter } from './filter';
-export { AdvancedFilter } from './filter/AdvancedFilter';
+export { AdvancedFilter } from './filter';
